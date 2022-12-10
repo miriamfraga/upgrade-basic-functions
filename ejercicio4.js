95c6aedc4b262e66132ef415888f243d8af3a243
“miriamfraga” <fragamr@outlook.com>
2022-12-10 18:42:05 +0100

    iteracion#4

---
+++ ejercicio4.js
@@ -0,0 +1,9 @@
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+function average(param) {
+    let promedio = 0; 
+    for (i=0; i <param.length; i++) {
+promedio += param[i] / param.length;
+    } console.log(promedio);
+  // insert code
+} 
+average(numbers);
