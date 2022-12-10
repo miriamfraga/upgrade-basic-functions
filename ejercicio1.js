152b52e62e73ec10ef1ac4fbf3036770b9ef1283
“miriamfraga” <fragamr@outlook.com>
2022-12-10 18:38:01 +0100

    node

---
+++ ejercicio1.js
@@ -0,0 +1,9 @@
+function sum(numberOne , numberTwo) {
+    let bigger;
+    if (numberOne>numberTwo) {
+        bigger = numberOne;
+    }else {
+        bigger =numberTwo;
+    } return bigger
+  }
+  sum (numberOne,numberTwo);
