298aeae0eb9d866e360d2c3b78c58d39b54fdd01
“miriamfraga” <fragamr@outlook.com>
2022-12-12 21:15:22 +0100

    Iteration#7

---
+++ ejercicio7.js
@@ -0,0 +1,35 @@
+/*Crea una función que reciba por parámetro un array y el valor 
+que desea comprobar que existe dentro de dicho array - comprueba 
+si existe el elemento, en caso que existan nos devuelve un true y 
+la posición de dicho elemento y por la contra un false. Puedes usar 
+este array para probar tu función: */
+
+
+
+const nameFinder = [
+    'Peter',
+    'Steve',
+    'Tony',
+    'Natasha',
+    'Clint',
+    'Logan',
+    'Xabier',
+    'Bruce',
+    'Peggy',
+    'Jessica',
+    'Marc'
+  ];
+  function finderName(array, valueToFind) {
+    let exists;
+    for(i=0; i<array.length; i++) {
+        if (array.includes(valueToFind)) {
+            exists= true;
+        }else {
+        exists =false;
+        }
+        
+} console.log(valueToFind + " " + exists);
+    return array.indexOf(valueToFind);
+} 
+ console.log(finderName(nameFinder, 'Natasha'));   
+
